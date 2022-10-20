967ec2d164273957ddd4fe80c8ce9e70576dadc0
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-20 19:10:07 +0300

    Comments were added to the file 'alien_invasion.py'.

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,9 @@ class AlienInvasion:
         # в пікселях. Це "поверхня" (surface) - частина екрана, де показується елемент гри.
         pygame.display.set_caption("Alien Invasion")
 
+        # Задати колір фону
+        self.bg_color = (100, 100, 120)  # red-green-blue
+
     def run_game(self):
         """Розпочати головний цикл гри."""
         while True:
@@ -24,6 +27,9 @@ class AlienInvasion:
                 # сталися після її останнього виклику.
                 if event.type == pygame.QUIT:  # Якщо гравець зачиняє вікно:
                     sys.exit()                 # Вихід з гри.
+
+            # Наново перемалювати екран на кожній ітерації циклу.
+            self.screen.fill(self.bg_color)
 
             # Показати останній намальований екран.
             pygame.display.flip()
