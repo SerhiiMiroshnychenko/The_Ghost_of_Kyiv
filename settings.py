b8ba4d850ae9d19bd3fe627d6bf4ae00219eb72e
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-22 15:10:59 +0300

    alien_invasion.py was modified (p.269).

--- settings.py
+++ settings.py
@@ -9,3 +9,8 @@ class Settings:
         self.bg_color = (100, 100, 120)  # Колір фона (red-green-blue)
         # Налаштування корабля
         self.ship_speed = 1.5
+        # Налаштування для кулі
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
