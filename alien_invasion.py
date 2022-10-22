58cefa389ea9ec00f5aa58d8d37406e141e7b22d
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-22 15:00:45 +0300

    alien_invasion.py was modified (p.267).

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # Перехід у повноекранний режим
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # Створює вікно, у якому будуть показуватися графічні елементи забавки.
         # Аргумент (self.settings.screen_width, self.settings.screen_height) - це кортеж, що позначає розміри вікна
         # завширшки та заввишки в пікселях.
